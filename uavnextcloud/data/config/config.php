6e6d145f0c60ae6bb3952824872ef6043ed09352
uav2608 <uav2608@gmail.com>
2019-03-01 04:51:52 +0100

    v7

---
+++ uavnextcloud/data/config/config.php
@@ -0,0 +1,39 @@
+<?php
+$CONFIG = array (
+  'htaccess.RewriteBase' => '/',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/html/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ),
+  ),
+  'instanceid' => 'ocr5juea0p6m',
+  'passwordsalt' => 'Ne9+uZCvkMkoSsNCiZAUPwlzx/2WM3',
+  'secret' => '8rLIAqPJnCq08uU8SIkjICeW2huFIDZFjVmfRix/Q0jbsZzC',
+  'trusted_domains' => 
+  array (
+    0 => '192.168.56.102:8081',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'mysql',
+  'version' => '15.0.5.3',
+  'overwrite.cli.url' => 'http://192.168.56.102:8081',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'mariadb:3306',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud',
+  'dbpassword' => 'Aropat0909',
+  'installed' => true,
+);
---
+++ uavnextcloud/data/version.php
@@ -0,0 +1,17 @@
+<?php 
+$OC_Version = array(15,0,5,3);
+$OC_VersionString = '15.0.5';
+$OC_Edition = '';
+$OC_Channel = 'stable';
+$OC_VersionCanBeUpgradedFrom = array (
+  'nextcloud' => 
+  array (
+    '14.0' => true,
+    '15.0' => true,
+  ),
+  'owncloud' => 
+  array (
+  ),
+);
+$OC_Build = '2019-02-27T20:47:13+00:00 5679410fef10e88e064dc08675e7fb1e52b7a7b3';
+$vendor = 'nextcloud';
